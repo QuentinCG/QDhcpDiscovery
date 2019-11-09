37452fcc142c34b9bd8e5f08358d9e5b4e99eb88
Quentin Comte-Gaz <quentin@comte-gaz.com>
2019-11-09 22:54:44 +0100

    Test: Add more unit-test case

--- test/main.cpp
+++ test/main.cpp
@@ -12,25 +12,60 @@
 
 #include <QCoreApplication>
 #include <QDebug>
+#include <QUdpSocket>
 
 #include <QDhcpDiscovery.h>
 
-TEST_CASE("Create and use instance")
+TEST_CASE("Invalid address")
 {
     QDhcpDiscovery dhcpDiscover;
     QString error;
 
-    REQUIRE_FALSE(dhcpDiscover.isAvailable(QHostAddress("127.0.01"), 10, error));
+    REQUIRE_FALSE(dhcpDiscover.isAvailable(QHostAddress(), 10, error));
     REQUIRE_FALSE(error.isEmpty());
+    qDebug() << "Returned error: " << error;
+    REQUIRE(error.contains("Invalid"));
+    REQUIRE(error.contains("interface"));
 }
 
-TEST_CASE("Use function without instance")
+TEST_CASE("Create and use instance with no DHCP server")
 {
     QDhcpDiscovery dhcpDiscover;
     QString error;
 
-    REQUIRE_FALSE(QDhcpDiscovery::isAvailable(QHostAddress("127.0.01"), 10, error));
+    REQUIRE_FALSE(dhcpDiscover.isAvailable(QHostAddress("127.0.0.1"), 10, error));
     REQUIRE_FALSE(error.isEmpty());
+    qDebug() << "Returned error: " << error;
+    REQUIRE(error.contains("No DHCP server found"));
+    REQUIRE(error.contains("Timeout"));
+}
+
+TEST_CASE("Use function without instance with no DHCP server")
+{
+    QDhcpDiscovery dhcpDiscover;
+    QString error;
+
+    REQUIRE_FALSE(QDhcpDiscovery::isAvailable(QHostAddress("127.0.0.1"), 10, error));
+    REQUIRE_FALSE(error.isEmpty());
+    qDebug() << "Returned error: " << error;
+    REQUIRE(error.contains("No DHCP server found"));
+    REQUIRE(error.contains("Timeout"));
+}
+
+TEST_CASE("Port already bind")
+{
+    QUdpSocket *udpServer = new QUdpSocket();
+    udpServer->bind(QHostAddress("127.0.0.1"), 68);
+
+    QDhcpDiscovery dhcpDiscover;
+    QString error;
+
+    REQUIRE_FALSE(QDhcpDiscovery::isAvailable(QHostAddress("127.0.0.1"), 1000, error));
+    REQUIRE_FALSE(error.isEmpty());
+    qDebug() << "Returned error: " << error;
+    REQUIRE(error.contains("Impossible to bind port 68"));
+
+    delete(udpServer);
 }
 
 int main(int argc, char* argv[])
